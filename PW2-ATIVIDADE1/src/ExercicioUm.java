1779c58e4e675203d6aa6c93d0e2c931f7f19a1b
thais.viana <thaisviana89@gmail.com>
2020-03-11 21:40:04 -0300

    Codificação de Método possoIrBanco()

--- PW2-ATIVIDADE1/src/ExercicioUm.java
+++ PW2-ATIVIDADE1/src/ExercicioUm.java
@@ -2,14 +2,14 @@
 public class ExercicioUm{
 	public static boolean ehDiaSemana(String fimDeSemana)
 	{
-		if (fimDeSemana == "Sábado" || fimDeSemana == "Domingo") {
+		if (fimDeSemana.equalsIgnoreCase("Sábado") || fimDeSemana.equalsIgnoreCase("Domingo")) {
 			return false;			
 		}
 		return true;
 	}
 
 	public static void main(String[] args) {
-		String diaSemana = "Domingo";	
+		String diaSemana = "Segunda-Feira";	
 		
 		if (ehDiaSemana(diaSemana) == true) {
 			System.out.println(diaSemana + " é dia de semana");
